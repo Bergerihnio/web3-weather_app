8d0596630840390abe0bc2a441a531f490e6a78a
Filip Berger <filipekbergerek@protonmail.com>
2024-06-26 13:55:12 +0200

    Create last hour temperature variable in API

--- raspberry/db_sqlite.py
+++ raspberry/db_sqlite.py
@@ -10,12 +10,14 @@ c.execute('''CREATE TABLE IF NOT EXISTS temperatures
               time TEXT DEFAULT (time('now', 'localtime')),
               date TEXT DEFAULT (date('now')))''')
 
-c.execute("SELECT temperature FROM temperatures ORDER BY date DESC")
 
-rows = c.fetchall()
+# c.execute("SELECT temperature FROM temperatures ORDER BY date DESC")
 
-for row in rows:
-    print(row[0])
+# rows = c.fetchall()
+
+
+# for row in rows:
+#     print(row[0])
 
 # c.execute("SELECT * FROM temperatures")
 # print(c.fetchall())
--- raspberry/flask-app.py
+++ raspberry/flask-app.py
@@ -14,7 +14,7 @@ scheduler.start()
 
 
 # Zadanie do pobierania i zapisywania temperatury co minute
-@scheduler.scheduled_job('interval', seconds=10)
+@scheduler.scheduled_job('interval', minutes=10)
 def save_schedule_job():
     temperature = thermometer.get_temperature()
     interia_temperature, pressure, wind, sunrise, sunset, humidity, rain_precipitation = web_scrapping.scrap_soup()
@@ -27,16 +27,31 @@ def save_to_db(interia_temperature):
     conn = sqlite3.connect('temperatures.db')
     c = conn.cursor()
     c.execute("INSERT INTO temperatures (temperature) VALUES (?)", (interia_temperature[:2],))
-    c.execute("SELECT * FROM temperatures")
-    print(c.fetchall())
+
     conn.commit()
     conn.close()
+
+def last_hour():
+    conn = sqlite3.connect('temperatures.db')
+    c = conn.cursor()
+    c.execute("SELECT temperature FROM temperatures ORDER BY date DESC")
+    rows = c.fetchall()
+
+    last_hour_temp = rows[0][0]
+
+    conn.commit()
+    conn.close()
+
+    return last_hour_temp
+    
 
 @app.route('/data', methods=['GET'])
 def get_data():
     temperature = thermometer.get_temperature()
 
     interia_temperature, pressure, wind, sunrise, sunset, humidity, rain_precipitation = web_scrapping.scrap_soup()
+
+    last_hour_temp = last_hour()
 
     return jsonify({
         'temperature': f'{temperature:.2f}',
@@ -46,7 +61,8 @@ def get_data():
         'interia_sunrise_time': sunrise,
         'interia_sunset_time': sunset,
         'humidity': humidity,
-        'rain_precipitation': rain_precipitation
+        'rain_precipitation': rain_precipitation,
+        'Last_hour_temperature': last_hour_temp
     })
 
 if __name__ == '__main__':
